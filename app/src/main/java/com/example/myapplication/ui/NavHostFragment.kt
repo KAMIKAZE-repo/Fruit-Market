d8da3a739833341223975854ef48d3237add656a
KAMIKAZE-repo <nassimboussami@gmail.com>
2021-06-25 21:55:48 +0100

    bottomnavbar fix!! or not

--- app/src/main/java/com/example/myapplication/ui/NavHostFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/NavHostFragment.kt
@@ -1,19 +1,27 @@
 package com.example.myapplication.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentNavHostBinding
 
 class NavHostFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_host, container, false)
+        val binding : FragmentNavHostBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_nav_host , container, false)
+        val navController = Navigation.findNavController(requireActivity(), R.id.home_fragment_container)
+        binding.bottomNavView.setupWithNavController(navController)
+        return binding.root
     }
 }
