ea8a9ee882e84b0c7fdfbc4237e904be1bf1eed0
KAMIKAZE-repo <nassimboussami@gmail.com>
2021-06-28 22:29:05 +0100

    well this is a fix :) bottomNavView Fixed

--- app/src/main/java/com/example/myapplication/ui/NavHostFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/NavHostFragment.kt
@@ -6,22 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentNavHostBinding
 
 class NavHostFragment : Fragment() {
+    private lateinit var binding : FragmentNavHostBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        val binding : FragmentNavHostBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_nav_host , container, false)
-        val navController = Navigation.findNavController(requireActivity(), R.id.home_fragment_container)
-        binding.bottomNavView.setupWithNavController(navController)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_nav_host , container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val host: NavHostFragment = childFragmentManager.findFragmentById(R.id.home_fragment_container) as NavHostFragment
+        binding.bottomNavView.setupWithNavController(host.navController)
+        NavigationUI.setupWithNavController(binding.toolbar, host.navController)
     }
 }
