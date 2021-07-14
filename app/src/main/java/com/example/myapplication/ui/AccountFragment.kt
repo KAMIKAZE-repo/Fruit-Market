8eb79e52f5a3af24b993256ed5e3b9c369c38526
KAMIKAZE-repo <nassimboussami@gmail.com>
2021-07-14 18:14:02 +0100

    favorite and order removed from account menu

--- app/src/main/java/com/example/myapplication/ui/AccountFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/AccountFragment.kt
@@ -5,14 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
+    private lateinit var binding: FragmentAccountBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        binding =  FragmentAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+        binding.settings.setOnClickListener {
+            navController.navigate(AccountFragmentDirections.actionAccountFragment2ToSettingsFragment())
+        }
+
+        binding.myFavorites.setOnClickListener {
+            navController.navigate(AccountFragmentDirections.actionAccountFragment2ToNavHostFragment(R.id.favorites))
+        }
+
+        binding.myCart.setOnClickListener {
+            navController.navigate(AccountFragmentDirections.actionAccountFragment2ToNavHostFragment(R.id.shoppingCartFragment))
+        }
+
+        binding.myOrders.setOnClickListener {
+            navController.navigate(AccountFragmentDirections.actionAccountFragment2ToNavHostFragment(R.id.myOrdersFragment))
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication/ui/AccountSettingsFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentAccountSettingsBinding
+
+class AccountSettingsFragment : Fragment() {
+    private lateinit var binding: FragmentAccountSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding =  FragmentAccountSettingsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+        NavigationUI.setupWithNavController(binding.toolbar, navController)
+    }
+}
